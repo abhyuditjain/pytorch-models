b478db0c3ab38493b7c75c373141e975c4461938
Abhyudit Jain <abhyuditjain@gmail.com>
2023-07-28 14:01:50 +0530

    added session11 transforms

--- utils/transforms.py
+++ utils/transforms.py
@@ -112,3 +112,40 @@ class CustomResnetTransforms:
 
     def __call__(self, img):
         return self.transformations(image=np.array(img))["image"]
+
+class Session11Transforms:
+    def __init__(self, means, stds, train=True):
+        if train:
+            self.transformations = Compose(
+                [
+                    Normalize(mean=means, std=stds, always_apply=True),
+                    Sequential(
+                        [
+                            PadIfNeeded(
+                                min_height=40, min_width=40, always_apply=True
+                            ),  # padding of 4 on each side of 32x32 image
+                            RandomCrop(height=32, width=32, always_apply=True),
+                        ]
+                    ),
+                    CoarseDropout(
+                        max_height=16,
+                        max_width=16,
+                        min_height=16,
+                        min_width=16,
+                        min_holes=1,
+                        max_holes=1,
+                        fill_value=means,
+                    ),
+                    ToTensorV2(),
+                ]
+            )
+        else:
+            self.transformations = Compose(
+                [
+                    Normalize(mean=means, std=stds, always_apply=True),
+                    ToTensorV2(),
+                ]
+            )
+
+    def __call__(self, img):
+        return self.transformations(image=np.array(img))["image"]
